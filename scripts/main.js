cded0e7562106d1d30a5dfba03cc47391fdd1785
nicwendell <nicolette.wendell@gmail.com>
2023-06-26 19:57:16 -0400

    restructured files

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "miss melon";
+
+// let myVariable = "melon"
+
+// this is a comment with no line breaks
+
+/*
+this is a comment with a line break
+*/
